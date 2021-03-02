647243b8cb5d8013de8a805764ec7c338acf4b97
dockofdoom <53832569+dockofdoom@users.noreply.github.com>
2021-03-02 13:37:20 -0600

    add hs-key and updated deploy.js file

--- deploy.js
+++ deploy.js
@@ -22,8 +22,8 @@ function installPM2() {
 // transfers local project to the remote server
 function transferProjectToRemote(failed, successful) {
   return ssh.putDirectory(
-    '../hackathon-starter',
-    '/home/ubuntu/hackathon-starter-temp',
+    '../starter-node-angular-master',
+    '/home/ubuntu/starter-node-angular-master-temp',
     {
       recursive: true,
       concurrency: 1,
@@ -49,7 +49,7 @@ function transferProjectToRemote(failed, successful) {
 // creates a temporary folder on the remote server
 function createRemoteTempFolder() {
   return ssh.execCommand(
-    'rm -rf hackathon-starter-temp && mkdir hackathon-starter-temp', {
+    'rm -rf starter-node-angular-master-temp && mkdir starter-node-angular-master-temp', {
       cwd: '/home/ubuntu'
   });
 }
@@ -65,7 +65,7 @@ function stopRemoteServices() {
 // updates the project source on the server
 function updateRemoteApp() {
   return ssh.execCommand(
-    'mkdir hackathon-starter && cp -r hackathon-starter-temp/* hackathon-starter/ && rm -rf hackathon-starter-temp', {
+    'mkdir starter-node-angular-master && cp -r starter-node-angular-master-temp/* starter-node-angular-master/ && rm -rf starter-node-angular-master-temp', {
       cwd: '/home/ubuntu'
   });
 }
@@ -73,7 +73,7 @@ function updateRemoteApp() {
 // restart mongodb and node services on the remote server
 function restartRemoteServices() {
   return ssh.execCommand(
-    'cd hackathon-starter && sudo service mongod start && pm2 start app.js', {
+    'cd starter-node-angular-master && sudo service mongod start && pm2 start app.js', {
       cwd: '/home/ubuntu'
   });
 }
